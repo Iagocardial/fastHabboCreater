6b23ee3a3b2553f9f7421fba73a3235c7fb926ef
Iago Cardial <iago.cardial@welbecare.com>
2023-09-20 21:17:13 -0300

    first commit

---
+++ emulator/scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+supervisord -c /app/supervisor/supervisord.conf
+
+cd /app/arcturus
+mvn package
+cp /app/config.ini /app/arcturus/target/config.ini
+mkdir /app/arcturus/target/plugins
+cd /app/arcturus/target/plugins
+wget https://cdn.discordapp.com/attachments/641088820598538252/873759980329766942/NitroWebsockets-3.1.jar
+
+supervisorctl start arcturus-emulator
+
+tail -f /dev/null
